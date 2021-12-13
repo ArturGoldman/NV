36ac3e68b00f3d4a4b68472880a9845434430ee6
ArturGoldman <arturgoldman184@gmail.com>
2021-12-13 18:11:35 +0300

    First launch

--- prep.sh
+++ prep.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 pip install torch==1.10.0+cu111 torchaudio==0.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
-
 pip install -r ./NV/requirements.txt
 
 wget https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
